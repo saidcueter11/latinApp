3bd306b6b12ec3df3b104263258f6c8148b6a72a
Said Cueter <rattion@pop-os.localdomain>
2022-12-13 18:20:38 -0500

    Added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomePageComponent} from "./pages/home-page/home-page.component";
-import {LoginComponent} from "./pages/login/login.component";
-import {RegisterComponent} from "./pages/register/register.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommunityPageComponent } from './components/community-page/community-page.component';
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { RegisterComponent } from "./pages/register/register.component";
 
 const routes: Routes = [
 
-  {path: '', component: LoginComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'home', component: HomePageComponent},
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'post', component: PostPageComponent },
+  { path: 'community', component: CommunityPageComponent }
 ];
 
 @NgModule({
